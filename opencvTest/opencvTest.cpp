85922ddcccd98b650d2bd4053f1078f46764b354
neutmute <neutmute.email>
2014-08-18 22:08:34 +1000

    64bit doesn't find file

--- opencvTest/opencvTest.cpp
+++ opencvTest/opencvTest.cpp
@@ -4,8 +4,31 @@
 #include "stdafx.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, const char** argv)
 {
+	string filename = "mypic.JPG";
+	Mat img = imread(filename, CV_LOAD_IMAGE_UNCHANGED); //read the image data in the file "MyPic.JPG" and store it in 'img'
+
+	if (img.empty()) //check whether the image is loaded or not
+	{
+
+		cout << "Error : Image cannot be loaded..!!" + filename << endl;
+		//system("pause"); //wait for a key press
+		return -1;
+	}
+
+	namedWindow("MyWindow", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+	imshow("MyWindow", img); //display the image which is stored in the 'img' in the "MyWindow" window
+
+	waitKey(0); //wait infinite time for a keypress
+
+	destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
+
 	return 0;
 }
-
