d06b53492490a11dd9c3909ab650a402aef4c3b3
neutmute <neutmute.email>
2014-08-18 23:17:33 +1000

    video works

--- opencvTest/opencvTest.cpp
+++ opencvTest/opencvTest.cpp
@@ -12,6 +12,9 @@ using namespace std;
 
 int main(int argc, const char** argv)
 {
+	/*
+	LOAD AN IMAGE
+
 	string filename = "mypic.JPG";
 	Mat img = imread(filename, CV_LOAD_IMAGE_UNCHANGED); //read the image data in the file "MyPic.JPG" and store it in 'img'
 
@@ -29,6 +32,46 @@ int main(int argc, const char** argv)
 	waitKey(0); //wait infinite time for a keypress
 
 	destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
+
+	return 0;
+	*/
+
+	VideoCapture cap("D:/Data/Documents/Pictures/!Temp/20140816_MarblePump/LegoMarblePump2_1.mp4"); // open the video file for reading
+
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the video file" << endl;
+		return -1;
+	}
+
+	//cap.set(CV_CAP_PROP_POS_MSEC, 300); //start the video at 300ms
+
+	double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
+
+	cout << "Frame per seconds : " << fps << endl;
+
+	namedWindow("MyVideo", CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+
+	while (1)
+	{
+		Mat frame;
+
+		bool bSuccess = cap.read(frame); // read a new frame from video
+
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read the frame from video file" << endl;
+			break;
+		}
+
+		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
 
 	return 0;
 }
