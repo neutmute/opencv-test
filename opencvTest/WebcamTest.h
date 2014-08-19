7f568cefb284f00ae27e73076134b855a71639c5
neutmute <neutmute.email>
2014-08-19 20:42:23 +1000

    webcam connection class

---
+++ opencvTest/WebcamTest.h
@@ -0,0 +1,9 @@
+#pragma once
+class WebcamTest
+{
+public:
+	WebcamTest();
+	~WebcamTest();
+	int Run();
+};
+
--- opencvTest/main.cpp
+++ opencvTest/main.cpp
@@ -5,16 +5,21 @@
 #include <iostream>
 #include "ImageLoadTest.h"
 #include "VideoFileTest.h"
+#include "WebcamTest.h"
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, const char** argv)
 {
-	/*VideoFileTest videoLoad;
-	return videoLoad.Run();*/
-
+	/*
 	ImageLoadTest imageLoad;
 	return imageLoad.Run();
 
+	VideoFileTest videoLoad;
+	return videoLoad.Run();
+	*/
+
+	WebcamTest webcamTest;
+	return webcamTest.Run();
 }
